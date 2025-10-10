8cd0441489cebfcedbd8bd6f26e2c8465ac236fe
Yassine Belkaid <yassine.belkaid87@gmail.com>
2025-10-10 18:49:44 +0300

    chore: update nginx to load upstream services

--- src/main/java/com/example/api/HealthController.java
+++ src/main/java/com/example/api/HealthController.java
@@ -1,5 +1,9 @@
 package com.example.api;
 
+// import java.util.Map;
+// import java.util.UUID;
+
+// import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +14,8 @@ public class HealthController {
 
     @GetMapping
     public String healthCheck() {
-        return "Application is running!";
+        // HttpHeaders responseHeaders = new HttpHeaders();
+        // responseHeaders.add("X-Report-ID", UUID.randomUUID().toString());
+        return "Application is running.";
     }
 }
